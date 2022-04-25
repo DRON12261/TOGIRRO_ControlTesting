56ebdbaf2815feac777ae345b757aa4b62ad8d0b
DRON12261 <andonovec2000@mail.ru>
2022-04-25 16:49:48 +0500

    Init repo

---
+++ TOGIRRO_ControlTesting/DBPlaceholders.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOGIRRO_ControlTesting
+{
+    static class Workfield
+    {
+        static public ObservableCollection<Subject> Subjects = new ObservableCollection<Subject> { };
+        static public ObservableCollection<Subject> ActiveSubjects = new ObservableCollection<Subject> { };
+		static public ObservableCollection<Quastion> Quastions = new ObservableCollection<Quastion> { };
+        static public WorkWindow workWindow = null;
+
+        static public void Init()
+        {
+            Subjects.Add(new Subject()
+            {
+                Name = "Русский язык",
+                Type = "ЕГЭ",
+                ChangeDate = DateTime.Now,
+                CreateDate = DateTime.Now,
+                Description = "Русский язык. 11 класс. 2019 г."
+            });
+			Subjects.Add(new Subject()
+			{
+				Name = "История",
+				Type = "ЕГЭ",
+				ChangeDate = DateTime.Now,
+				CreateDate = DateTime.Now,
+				Description = "История. 11 класс. 2019 г."
+			});
+			Subjects.Add(new Subject()
+			{
+				Name = "Обществознание",
+				Type = "ОГЭ",
+				ChangeDate = DateTime.Now,
+				CreateDate = DateTime.Now,
+				Description = "Обществознание. 9 класс. 2020 г."
+			});
+
+			Quastions.Add(new Quastion()
+			{
+				N1 = "1",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "2"
+			});
+			Quastions.Add(new Quastion()
+			{
+				N1 = "2",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "1"
+			});
+			Quastions.Add(new Quastion()
+			{
+				N1 = "3",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "1"
+			});
+			Quastions.Add(new Quastion()
+			{
+				N1 = "4",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "3"
+			});
+			Quastions.Add(new Quastion()
+			{
+				N1 = "5",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "4"
+			});
+			Quastions.Add(new Quastion()
+			{
+				N1 = "6",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "2"
+			});
+			Quastions.Add(new Quastion()
+			{
+				N1 = "7",
+				N2 = "Одиночный выбор ответа",
+				N3 = "1",
+				N4 = "3"
+			});
+		}
+    }
+
+    enum SubjectType
+    {
+        Type1 = 1,
+        Type2,
+        Type3,
+        Type4
+    }
+
+    enum QuestionType
+    {
+        Type1 = 1,
+        Type2,
+        Type3,
+        Type4
+    }
+
+	class Quastion
+	{
+		public string N1 { get; set; }
+		public string N2 { get; set; }
+		public string N3 { get; set; }
+		public string N4 { get; set; }
+	}
+
+    class Subject
+    {
+        public string Name { get; set; }//"SubjectName";
+        public DateTime CreateDate { get; set; }//= DateTime.Now;
+        public DateTime ChangeDate { get; set; }//= DateTime.Now;
+        public string Description { get; set; }//= "SubjectDescription";
+        public string Type { get; set; }//= SubjectType.Type1;
+        public ObservableCollection<Variant> Variants = null;
+
+        public Subject()
+        {
+            Variants = new ObservableCollection<Variant> { new Variant(this) };
+        }
+    }
+
+    class Variant
+    {
+        public string Name { get; set; }
+        public Subject ParentSubject = null;
+        public ObservableCollection<Chapter> Chapters = null;
+
+        public Variant(Subject parentSubject)
+        {
+            ParentSubject = parentSubject;
+            Name = "Variant 1";
+            Chapters = new ObservableCollection<Chapter> { new Chapter(this) };
+        }
+    }
+
+    class Chapter
+    {
+        public string Name { get; set; }
+        public Variant ParentVariant = null;
+        public ObservableCollection<Question> Questions = null;
+
+        public Chapter(Variant parentVariant)
+        {
+            ParentVariant = parentVariant;
+            Name = "Chapter 1";
+            Questions = new ObservableCollection<Question> { new Question(this) };
+        }
+    }
+
+    class Question
+    {
+        public string Name { get; set; }
+        public QuestionType Type = QuestionType.Type1;
+        public Chapter ParentChapter = null;
+        public int MaxScore = 1;
+
+        public Question(Chapter parentChapter)
+        {
+            Name = "Question 1";
+            ParentChapter = parentChapter;
+        }
+    }
+}
---
+++ TOGIRRO_ControlTesting/SubjectWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TOGIRRO_ControlTesting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class SubjectWindow : Window
+    {
+        public SubjectWindow()
+        {
+            InitializeComponent();
+
+            CreateSubject_Type.ItemsSource = Enum.GetValues(typeof(SubjectType)).Cast<SubjectType>();
+            CreateSubject_Type.SelectedIndex = 0;
+
+            EditSubject_Type.ItemsSource = Enum.GetValues(typeof(SubjectType)).Cast<SubjectType>();
+            EditSubject_Type.SelectedIndex = 0;
+
+            Workfield.Init();
+            SubjectsList.ItemsSource = Workfield.Subjects;
+        }
+
+        private void ButtonC_CreateSubject(object sender, RoutedEventArgs e)
+        {
+            CreateSubject_Name.Text = "";
+            CreateSubject_Description.Text = "";
+            CreateSubject_Type.SelectedIndex = 0;
+            CreateTab.IsSelected = true;
+        }
+
+        private void ButtonC_EditSubject(object sender, RoutedEventArgs e)
+        {
+            Subject SelectedSubject = (Subject)SubjectsList.SelectedItem;
+            EditSubject_Name.Text = SelectedSubject.Name;
+            EditSubject_Description.Text = SelectedSubject.Description;
+            //EditSubject_Type.SelectedIndex = (int)(SelectedSubject.Type) - 1;
+            ICollectionView view = CollectionViewSource.GetDefaultView(Workfield.Subjects);
+            view.Refresh();
+            EditTab.IsSelected = true;
+        }
+
+        private void ButtonC_DeleteSubject(object sender, RoutedEventArgs e)
+        {
+            Subject SelectedSubject = (Subject)SubjectsList.SelectedItem;
+            Workfield.Subjects.Remove(SelectedSubject);
+        }
+
+        private void ButtonC_CreateSubject_Create(object sender, RoutedEventArgs e)
+        {
+            if (CreateSubject_Name.Text.Trim(' ') != "")
+            {
+                /*Workfield.Subjects.Add(new Subject() {  Name = CreateSubject_Name.Text.Trim(' '), 
+                                                        Type = (SubjectType)(CreateSubject_Type.SelectedIndex+1), 
+                                                        CreateDate = DateTime.Now, 
+                                                        ChangeDate = DateTime.Now,
+                                                        Description = CreateSubject_Description.Text.Trim(' ')});*/
+                ListTab.IsSelected = true;
+            }
+        }
+
+        private void ButtonC_CreateSubject_Cancel(object sender, RoutedEventArgs e)
+        {
+            ListTab.IsSelected = true;
+        }
+
+        private void ButtonC_EditSubject_Edit(object sender, RoutedEventArgs e)
+        {
+            if (CreateSubject_Name.Text.Trim(' ') != "")
+            {
+                Subject SelectedSubject = (Subject)SubjectsList.SelectedItem;
+                SelectedSubject.Name = EditSubject_Name.Text;
+                SelectedSubject.Description = EditSubject_Description.Text;
+                //SelectedSubject.Type = (SubjectType)(EditSubject_Type.SelectedIndex+1);
+                ListTab.IsSelected = true;
+            }
+        }
+
+        private void ButtonC_EditSubject_Cancel(object sender, RoutedEventArgs e)
+        {
+            ListTab.IsSelected = true;
+        }
+
+        private void ButtonC_LaunchSubject(object sender, RoutedEventArgs e)
+        {
+            Subject SelectedSubject = (Subject)SubjectsList.SelectedItem;
+            Workfield.ActiveSubjects.Add(SelectedSubject);
+            Workfield.workWindow = new WorkWindow();
+            Workfield.workWindow.Show();
+        }
+    }
+}
